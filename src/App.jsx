14f240154b47810d34a467b4bbb7edb6ec58c98b
sashantnipate <sashantnipate@gmail.com>
2025-12-09 17:58:22 +0530

    Just some Style add

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import SearchBar from './components/SearchBar/SearchBar';
-import SearchResults from './components/SearchResults/SearchResults';
-import Playlist from './components/Playlist/Playlist';
-import React , {useState} from 'react';
+import React, { useState } from "react";
+import "./App.css";               
+import SearchBar from "./components/SearchBar/SearchBar";
+import SearchResults from "./components/SearchResults/SearchResults";
+import Playlist from "./components/Playlist/Playlist";
 
-function App(){
+function App() {
   const [searchResults, setSearchResults] = useState([]);
 
-  const fackTrack = [
-    {id:"1", name:"Song A", artist: "Artist A", album: "Album A"},
-    {id:"2", name:"Song B", artist: "Artist B", album: "Album B"}
+  const fakeTrack = [
+    { id: "1", name: "Song A", artist: "Artist A", album: "Album A" },
+    { id: "2", name: "Song B", artist: "Artist B", album: "Album B" }
   ];
 
-  function handleSearch(){
-    setSearchResults(fackTrack);
+  function handleSearch() {
+    setSearchResults(fakeTrack);
   }
 
-  return(
-    <>
+  return (
+    <div className="app-container">
       <h1>Jammming</h1>
-      <SearchBar onSearchClick = {handleSearch}/>
-      <SearchResults searchResults = {searchResults}/>
-      <Playlist/>
-    </>
+      <SearchBar onSearchClick={handleSearch} className = "search-bar"/>
+      <SearchResults searchResults={searchResults} />
+      <Playlist />
+    </div>
   );
 }
 
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,20 @@
+import styles from "./SearchBar.module.css";
 
-function SearchBar(props){
-    return(
-        <div>
-            <input placeholder="Enter a song, album, or artist"></input>
-            <button onClick = {props.onSearchClick}>Search</button>
-        </div>
-    )
+function SearchBar(props) {
+  return (
+    <div className={styles.searchBar}>
+      <input
+        className={styles.searchInput}
+        placeholder="Enter a song, album, or artist"
+      />
+      <button
+        className={styles.searchButton}
+        onClick={props.onSearchClick}
+      >
+        Search
+      </button>
+    </div>
+  );
 }
 
 export default SearchBar;
